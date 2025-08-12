2141c90742091642713ab2deffdce4279f16eb7d
yats1304 <chaubaly@gmail.com>
2025-08-12 10:55:41 +0530

    enter output reflect on page using useState

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,22 +3,32 @@ import Button from '../Button/Button';
 import styles from './ContactForm.module.css';
 import { FaPhoneAlt } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
-
+import { useState } from 'react';
 
 const ContactForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [text, setText] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setName(event.target[0].value);
+    setEmail(event.target[1].value);
+    setText(event.target[2].value);
+  }
   return (
     <section className={styles.container}>
         <div className={styles.contact_form}>
           <div className={styles.top_btn}>
           <Button text="VIA SUPPORT CHAT" icon={<MdMessage fontSize="24px"/>}/>
-          <Button text="VIA CALL" icon={<FaPhoneAlt fontSize="24px"/>}/>
+          <Button 
+          text="VIA CALL" icon={<FaPhoneAlt fontSize="24px"/>}/>
           </div>
           <Button 
           isOutline= {true}
           text="VIA EMAIL FORM" 
           icon={<HiMail fontSize="24px"/>}/>
           
-          <form> 
+          <form onSubmit={onSubmit}> 
             <div className={styles.form_control}>
               <label htmlFor='name'>Name</label>
               <input type='text' name='name'/>
@@ -36,6 +46,11 @@ const ContactForm = () => {
               justifyContent: "end"
             }}>
               <Button text="SUBMIT"/>
+            </div>
+            <div className='output'>
+              <p>Name: {name}</p>
+              <p>Email: {email}</p>
+              <p>Text: {text}</p>
             </div>
           </form>
 
